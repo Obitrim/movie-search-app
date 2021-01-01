ec30d7b7c20f703194375ae4ed090b528f8c43fa
obitrim <pevangelist516@gmail.com>
2021-01-01 22:39:41 +0000

    Updates

---
+++ precache-manifest.b359c48d8d3428eadcd6b5c6ea6feb9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d78f37d0d67e3d3a68ccb57ab2dbff2",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "248888b919fdf13c449f",
+    "url": "/movie-search-app/static/css/main.7a618596.chunk.css"
+  },
+  {
+    "revision": "43922c9b03c4812dd547",
+    "url": "/movie-search-app/static/js/2.a789de42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.a789de42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248888b919fdf13c449f",
+    "url": "/movie-search-app/static/js/main.a8c58fe7.chunk.js"
+  },
+  {
+    "revision": "f49be466c2978a4142a9",
+    "url": "/movie-search-app/static/js/runtime-main.4194dbf0.js"
+  }
+]);
