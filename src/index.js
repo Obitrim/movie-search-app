727dccd55507f5bbe86f08c67e871e1bab71ed3a
obitrim <pevangelist516@gmail.com>
2021-01-02 21:06:31 +0000

    initialized animate on scroll library

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './styles/index.css';
+import './index.css';
 import App from './views/App';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<BrowserRouter>
+	    <App />
+	</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
