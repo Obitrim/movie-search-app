563bd2e7b644c049c244b731ee6ce260596cf333
obitrim <pevangelist516@gmail.com>
2021-01-03 14:55:14 +0000

    removed Search Link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 import "./Navbar.css";
 
 const Index = (props) => {
   return (
     <nav className="Navbar">
-    	<NavLink to="/" activeClassName="NavLink--Active" className="NavLink" exact>Home</NavLink>
-    	<NavLink to="/favorite" activeClassName="NavLink--Active" className="NavLink">Favorite</NavLink>
-    	<NavLink to="/search" activeClassName="NavLink--Active" className="NavLink">Search</NavLink>
-    	<NavLink to="/profile" activeClassName="NavLink--Active" className="NavLink">Profile</NavLink>
+    	<NavLink to="/" activeClassName="NavLink--Active" className="NavLink" exact>
+    		<HomeIcon/>
+    		Home
+    	</NavLink>
+    	<NavLink to="/favorite" activeClassName="NavLink--Active" className="NavLink">
+    		<FavoriteIcon/>
+    		Favorites
+    	</NavLink>
+    	<NavLink to="/profile" activeClassName="NavLink--Active" className="NavLink NavLink--Profile">
+    		<AccountCircleIcon/>
+    	</NavLink>
     </nav>
   )
 }
