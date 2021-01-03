8d925d8fe7991ab0dc5144e19d81ba1033440740
obitrim <pevangelist516@gmail.com>
2021-01-03 14:51:04 +0000

    deleted file

--- src/views/Favorite/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = (props) => {
-  return (
-    <div>Favorite view</div>
-  )
-}
-
-export default Index;
--- src/views/Search/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = (props) => {
-  return (
-    <div>Search view</div>
-  )
-}
-
-export default Index;
