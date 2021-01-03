1ea073eac6a567c3b185a82996dd05bff69f4480
obitrim <pevangelist516@gmail.com>
2021-01-03 14:50:01 +0000

    renamed directory for favorites view

---
+++ src/views/Favorites/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Favorites.css';
+
+const Index = (props) => {
+  return (
+    <section className="Favorites">
+    	<h1>Favorites</h1>
+    </section>
+  )
+}
+
+export default Index;
